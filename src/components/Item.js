a4afee69772eeac2af0819654e24a314897eafba
el-sam <samurai.h@yahoo.com>
2023-12-20 12:10:50 +0800

    use the state variable to set the dynamic look for both item cart status and the button message

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,19 @@
 import React, {useState} from "react";
 
 function Item({ name, category }) {
+  const [inCart,setInCart] = useState("false");
+
+  function handleAddClick (){
+    setInCart((inCart) => !inCart)
+  }
+  
+  const cartStatus = inCart ? "" : "in-cart"
+  const cartMsg = inCart ? "Add to Cart" : "Remove From Cart"
   return (
-    <li className="">
+    <li className={cartStatus}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className="add" onClick={handleAddClick}>{cartMsg}</button>
     </li>
   );
 }
